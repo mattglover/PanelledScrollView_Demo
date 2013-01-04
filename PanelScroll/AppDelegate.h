45a1a8790df2f1637d57b2145e4b50eff34ce120
Matt Glover <matt@duchysoftware.com>
2013-01-04 08:55:21 +0000

    Initial Commit

---
+++ PanelScroll/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PanelScroll
+//
+//  Created by Matt Glover on 04/01/2013.
+//  Copyright (c) 2013 Duchy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PanelScroll/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PanelScroll
+//
+//  Created by Matt Glover on 04/01/2013.
+//  Copyright (c) 2013 Duchy Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
